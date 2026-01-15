3d9875605a7d7137da40de5153526ace3c25f6df
vinay-kumar-123 <kingboy92785@gmail.com>
2026-01-16 00:35:55 +0530

    dashboard update

--- Server/app/routers/dashboard.py
+++ Server/app/routers/dashboard.py
@@ -1,31 +1,57 @@
+# from fastapi import APIRouter
+# from app.services.order_service import get_today_orders
+
+
+# router = APIRouter(prefix="/dashboard", tags=["Dashboard"])
+
+
+# @router.get("/org/today/{org_id}/{menu_id}")
+# def today_summary(org_id: str, menu_id: str):
+#     orders = get_today_orders(org_id, menu_id)
+
+#     eaten = 0
+#     skipped = 0
+
+#     for o in orders:
+#         for i in o["items"]:
+#             if i["status"] == "Eat":
+#                 eaten += 1
+#             else:
+#                 skipped += 1
+
+#     total = eaten + skipped
+
+#     return {
+#         "foodEaten": eaten,
+#         "foodSkipped": skipped,
+#         "responses": len(orders),
+#         "efficiency": round((eaten / total) * 100, 2) if total else 0,
+#         "orders": [
+#             {
+#                 "studentId": o["studentId"],
+#                 "studentName": o["studentName"],
+#                 "items": o["items"]
+#             }
+#             for o in orders
+#         ]
+#     }
+
 from fastapi import APIRouter
 from app.services.order_service import get_today_orders
-from bson import ObjectId
 
 router = APIRouter(prefix="/dashboard", tags=["Dashboard"])
-
 
 @router.get("/org/today/{org_id}/{menu_id}")
 def today_summary(org_id: str, menu_id: str):
     orders = get_today_orders(org_id, menu_id)
 
-    eaten = 0
-    skipped = 0
-    safe_orders = []
-
+    eaten = skipped = 0
     for o in orders:
         for i in o["items"]:
             if i["status"] == "Eat":
                 eaten += 1
             else:
                 skipped += 1
-
-        safe_orders.append({
-            "orderId": str(o["_id"]),        
-            "studentId": o["studentId"],
-            "studentName": o["studentName"],
-            "items": o["items"]
-        })
 
     total = eaten + skipped
 
@@ -34,5 +60,6 @@ def today_summary(org_id: str, menu_id: str):
         "foodSkipped": skipped,
         "responses": len(orders),
         "efficiency": round((eaten / total) * 100, 2) if total else 0,
-        "orders": safe_orders              
+        "orders": orders
     }
+
