1e8de8fdfa26005b5aed9c599c3a04cd7cdcf1b8
vinay-kumar-123 <kingboy92785@gmail.com>
2026-04-15 03:14:11 +0530

    some file are deleted

--- Server/check.py
+++
@@ -1,6 +0,0 @@
-from app.db.mongodb import db
-import json
-
-indexes = list(db.orders.list_indexes())
-
-print(json.dumps(indexes, indent=2, default=str))
--- Server/create.py
+++
@@ -1,3 +0,0 @@
-from app.db.create_indexes import create_indexes
-
-create_indexes()
